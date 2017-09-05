55c827a9577eaca65009517aff5dd02b9d13a557
Anton <anton@ondoc.me>
2017-09-05 22:46:53 +0300

    Попытка пофиксить баг

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'sidekiq/web'
+require_relative 'workers/event_worker'
 
 post '/event' do
   EventWorker.perform_async(params[:event])
+end
+
+get '/sidekiq' do
+  run Sidekiq::Web
 end
