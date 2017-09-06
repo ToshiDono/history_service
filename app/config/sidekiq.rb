37d25f73c07cd206d3df27d795e23ee85a53fc04
Anton <anton@ondoc.me>
2017-09-06 22:43:03 +0300

    Fix db configs

--- app/config/sidekiq.rb
+++ app/config/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { db: 1, url: "redis://#{ENV['DB_HOST']}:#{ENV['DB_PORT']}", network_timeout: 5 }
+  config.redis = { db: 1, url: "redis://#{ENV['DB_HOST']}:#{ENV['DB_REDIS_PORT']}", network_timeout: 5 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { db: 1, url: "redis://#{ENV['DB_HOST']}:#{ENV['DB_PORT']}", network_timeout: 5 }
+  config.redis = { db: 1, url: "redis://#{ENV['DB_HOST']}:#{ENV['DB_REDIS_PORT']}", network_timeout: 5 }
 end
--- app/db.rb
+++ app/db.rb
@@ -5,6 +5,6 @@ DB = Sequel.connect(
                user: ENV['DB_USER'],
                password: ENV['DB_PASSWORD'],
                host: ENV['DB_HOST'],
-               port: ENV['DB_PORT'],
+               port: ENV['DB_PG_PORT'],
                database: ENV['DB']
 )
