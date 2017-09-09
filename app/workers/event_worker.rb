9e006cbd51013aaf04b999221d95823b53064ad2
Anton <anton@ondoc.me>
2017-09-09 23:55:20 +0300

    Refactor EventWorker

--- app/workers/event_worker.rb
+++ app/workers/event_worker.rb
@@ -5,15 +5,7 @@ class EventWorker
 
   def perform(event)
     performed_actions = DB.from(:performed_actions)
-    performed_actions.insert(
-        actor_id: event['actor_id'],
-        subject_id: event['subject_id'],
-        actor_type: event['actor_type'],
-        subject_type: event['subject_type'],
-        action: event['action'],
-        created_at: Time.now,
-        updated_at: Time.now
-    )
+    performed_actions.insert(event.merge(created_at: Time.now, updated_at: Time.now))
     incr_event
   end
 
