2dd0efe27c068a313d5a84fd6ded383f2c7ac458
Anton <anton@ondoc.me>
2017-09-09 18:01:30 +0300

    Update view data

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,6 @@ end
 get '/events' do
   page = params.fetch "page", 1
   per_page = 100
-  @pa = DB.from(:performed_actions).extension(:pagination).paginate(page.to_i, per_page.to_i)
+  @performed_actions = DB.from(:performed_actions).extension(:pagination).paginate(page.to_i, per_page.to_i)
   erb :events
 end
