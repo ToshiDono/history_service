ad2f3c728ae3bf49d99107030befb61cf01b9dd2
Anton <anton@ondoc.me>
2017-09-10 18:58:53 +0300

    Fix bug in History Redis

--- config/redis.rb
+++ config/redis.rb
@@ -12,9 +12,7 @@ require 'singleton'
 class HistoryRedis
   include Singleton
 
-    @connect = Redis.new(host: ENV['DB_HOST'], port: ENV['DB_REDIS_PORT'], db: 1,timeout: 1)
-
   def connection
-    @connect
+    Redis.new(host: ENV['DB_HOST'], port: ENV['DB_REDIS_PORT'], db: 1,timeout: 1)
   end
 end
