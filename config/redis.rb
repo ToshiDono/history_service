8650036af3a6c315e14bf51127ee97726976d8c2
Anton <anton@ondoc.me>
2017-09-10 18:37:20 +0300

    Create new version HistoryRedis Singleton

--- config/redis.rb
+++ config/redis.rb
@@ -1,9 +1,20 @@
 require 'redis'
+require 'singleton'
 
-module HistoryRedis
-  @connect = Redis.new(host: ENV['DB_HOST'], port: ENV['DB_REDIS_PORT'], db: 1,timeout: 1)
+# module HistoryRedis
+#   @connect = Redis.new(host: ENV['DB_HOST'], port: ENV['DB_REDIS_PORT'], db: 1,timeout: 1)
+#
+#   def self.connection
+#     @connect
+#   end
+# end
 
-  def self.connection
+class HistoryRedis
+  include Singleton
+
+    @connect = Redis.new(host: ENV['DB_HOST'], port: ENV['DB_REDIS_PORT'], db: 1,timeout: 1)
+
+  def connection
     @connect
   end
 end
