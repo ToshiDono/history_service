fcd12eecfc6daf757f6403311a3589b76ddc4bea
Anton <anton@ondoc.me>
2017-09-11 23:45:58 +0300

    Попытка сделать хеш в редис

--- app/app.rb
+++ app/app.rb
@@ -4,13 +4,13 @@ require 'sequel/extensions/pagination'
 
 post '/event' do
   EventWorker.perform_async(params)
-  set_cache
+  # set_cache
 end
 
 get '/events' do
-  unless cache_exists?
-    set_cache
-  end
+  # unless cache_exists?
+  #   set_cache
+  # end
   per_page = 100
   # events
   @performed_actions = DB.from(:performed_actions).extension(:pagination).paginate(page, per_page)
@@ -26,7 +26,19 @@ def cache_exists?
 end
 
 def set_cache
-  Sidekiq.redis { |con| con.set('events_cache', 'cache') }
+  # Sidekiq.redis { |con| con.set('events_cache', 'cache') }
+  p_a = DB.from(:performed_actions).extension(:pagination).paginate(1, 100)
+  Sidekiq.redis do |con|
+    p_a.each do |row|
+      con.hmset('events_cache',
+                'actor_id', row[:actor_id],
+                'actor_type', row[:actor_type],
+                'action', row[:action],
+                'subject_type', row[:subject_type],
+                'created_at', row[:created_at]
+      )
+    end
+  end
 end
 
 def events
@@ -38,9 +50,32 @@ def events
 end
 
 def events_from_cache
-
+   Sidekiq.redis do |con|
+    con.hgetall('events_cache',
+                @performed_actions[:actor_id],
+                @performed_actions[:actor_type],
+                @performed_actions[:action],
+                @performed_actions[:subject_type],
+                @performed_actions[:created_at],
+    )
+  end
 end
 
 def events_from_db
+  per_page = 100
+  @performed_actions = DB.from(:performed_actions).extension(:pagination).paginate(page, per_page)
+end
 
+def puts_events_cache
+  puts "DEBUG"
+  puts "DEBUG"
+  puts "DEBUG"
+  Sidekiq.redis { |con| puts con.get('events_cache').inspect }
+  puts "DEBUG"
+  puts "DEBUG"
+  puts "DEBUG"
+end
+
+def del_cache
+  Sidekiq.redis { |con| con.del('events_cache') }
 end
