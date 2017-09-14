54270c89f0d33d239c05e90c3d89a50b3fa583ea
Anton <anton@ondoc.me>
2017-09-14 22:21:51 +0300

    Refactor Cache classes

--- app/cache/base.rb
+++ app/cache/base.rb
@@ -2,8 +2,38 @@ require_relative '../../config/redis'
 
 class Cache
   class Base
-    def list_exists?(list)
-      redis_connect { |con| con.llen(list) != 0 }
+
+    # checks the presence of a cache by the presence of a list with keys
+    def list_exists?
+      redis_connect { |con| con.llen(list_name) != 0 }
+    end
+
+    # return []
+    def list
+      redis_connect do |con|
+        list_size = con.llen(list_name)
+        list = con.lrange(list_name, 0, list_size)
+        list
+      end
+    end
+
+    # return [{hash}]
+    def hashes
+      hashes = []
+      redis_connect do |con|
+        list.each do  |item|
+          hashes << con.hgetall(hash_name(item))
+        end
+      end
+      hashes
+    end
+
+    # removes hashes and list
+    def destroy_cache
+      list.each do |id|
+        destroy_hash(id)
+      end
+      destroy_list
     end
 
     private
@@ -15,14 +45,20 @@ class Cache
         arr << hash[key.to_sym]
       end
       redis_connect do |con|
-        con.hmset("cache:events:#{hash[:id]}",
-            arr
-        )
+        con.hmset("cache:events:#{hash[:id]}", arr)
       end
     end
 
-    def add_hash_to_list(list, id)
-      redis_connect { |con| con.rpush(list, id) }
+    def add_hash_to_list(id)
+      redis_connect { |con| con.rpush(list_name, id) }
+    end
+
+    def destroy_list
+      redis_connect { |con| con.del(list_name) }
+    end
+
+    def destroy_hash(id)
+      redis_connect { |con| con.del(hash_name(id)) }
     end
 
     # return Redis.connect
--- app/cache/performed_action.rb
+++ app/cache/performed_action.rb
@@ -5,43 +5,18 @@ require_relative 'base'
 class Cache
   class PerformedAction < Cache::Base
 
-    # checks the presence of a cache by the presence of a list with keys
-    def list_exists?
-      super('cache:events:list')
-    end
-
     # create hash and add  add id hash to events list
     def create_event(performed_action)
       create_events_hash(performed_action)
-      add_events_hash_to_list performed_action[:id]
+      add_hash_to_list(performed_action[:id])
     end
 
-    # return [{event}]
-    def events
-      @performed_actions = []
-      redis_connect do |con|
-        events_ids.each do  |event|
-          @performed_actions << con.hgetall("cache:events:#{event}")
-        end
-      end
-      @performed_actions
+    def list_name
+      'cache:events:list'
     end
 
-    # return [events list]
-    def events_ids
-      redis_connect do |con|
-        events_size = con.llen('cache:events:list')
-        events_list = con.lrange('cache:events:list', 0, events_size)
-        events_list
-      end
-    end
-
-    # removes hashes and event list
-    def destroy_events_cache
-      events_ids.each do |event_id|
-        destroy_event(event_id)
-      end
-      destroy_events_list
+    def hash_name(id)
+      "cache:events:#{id}"
     end
 
     private
@@ -51,29 +26,5 @@ class Cache
       keys= ['actor_id', 'actor_type', 'action', 'subject_id', 'subject_type', 'created_at']
       create_hash(performed_action, keys)
     end
-
-    # add id hash to events list
-    def add_hash_to_list(id)
-      super('cache:events:list', id)
-    end
-
-    # deletes the list of events
-    def destroy_events_list
-      redis_connect { |con| con.del('cache:events:list') }
-    end
-
-    # deletes the hash of events
-    def destroy_event(id)
-      redis_connect { |con| con.del("cache:events:#{id}") }
-    end
-
-    # # return Redis.connect
-    # def redis_connect(*args, &b)
-    #   con = HistoryRedis.instance.connection
-    #   # Sidekiq.redis { |con| yield(con, *args) }
-    #   yield(con, *args)
-    # end
   end
 end
-
-
