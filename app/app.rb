fd186b5e8ad063dd6d93e0ebbb8afd6e16538148
Anton <anton@ondoc.me>
2017-09-16 22:48:37 +0300

    Попытка сделать пагинацию kaminari

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'kaminari'
 require_relative '../config/application'
 require_relative 'dto/performed_action'
 require 'sequel/extensions/pagination'
@@ -14,7 +15,7 @@ end
 
 # return[Dto::PerformedAction]
 def events
-  collection = if page == 1 or nil
+  collection = if params_page == 1 or nil
                  cached_events || events_from_db
                else
                  events_from_db
@@ -22,7 +23,7 @@ def events
   to_dto(collection)
 end
 
-def page
+def params_page
   (params.fetch "page", 1).to_i
 end
 
@@ -33,7 +34,7 @@ end
 
 def events_from_db
   per_page = 100
-  DB.from(:performed_actions).extension(:pagination).paginate(page, per_page).to_a
+  Kaminari.paginate_array(DB.from(:performed_actions).to_a).page(params_page).per(per_page)
 end
 
 def to_dto(collection)
@@ -49,8 +50,12 @@ def puts_debug(elem)
   puts "DEBUG"
   puts "DEBUG"
   puts elem.class
-  puts elem.inspect
-  puts elem[0] if elem.class == 'Array'
+  puts "DEBUG"
+  puts "DEBUG"
+  puts elem.size
+  puts "DEBUG"
+  puts "DEBUG"
+  puts params.inspect
   puts "DEBUG"
   puts "DEBUG"
 end
