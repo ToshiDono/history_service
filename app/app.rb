0fb4af7c280b4b524a05ee3d31f6b9c486392c77
Anton <anton@ondoc.me>
2017-09-16 18:07:51 +0300

    Попытался сделать вывод dto

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative '../config/application'
+require_relative 'dto/performed_action'
 require 'sequel/extensions/pagination'
 
 post '/event' do
@@ -11,12 +12,17 @@ get '/events' do
   erb :events
 end
 
+# return[Dto::PerformedAction]
 def events
-  if page == 1
-    cached_events || events_from_db
-  else
-    events_from_db
-  end
+  collection = if page == 1
+                 cached_events || events_from_db
+               else
+                 events_from_db
+               end
+  puts_debug(collection)
+  collection = to_dto(collection)
+  puts_debug(collection)
+  collection
 end
 
 def page
@@ -30,17 +36,24 @@ end
 
 def events_from_db
   per_page = 100
-  DB.from(:performed_actions).extension(:pagination).paginate(page, per_page)
+  DB.from(:performed_actions).extension(:pagination).paginate(page, per_page).to_a
+end
+
+def to_dto(collection)
+  dto_events = []
+  collection.each do |elem|
+    dto_events << Dto::PerformedAction.new(elem)
+  end
+  dto_events
 end
 
 
-
-def puts_events_cache
+def puts_debug(elem)
   puts "DEBUG"
   puts "DEBUG"
-  puts "DEBUG"
-  Sidekiq.redis { |con| puts con.get('events_cache').inspect }
-  puts "DEBUG"
+  puts elem.class
+  puts elem.inspect
+  puts elem[0] if elem.class == 'Array'
   puts "DEBUG"
   puts "DEBUG"
 end
--- app/dto/performed_action.rb
+++ app/dto/performed_action.rb
@@ -3,8 +3,8 @@ class Dto
     attr_reader :actor_id, :actor_type, :subject_id, :subject_type, :action, :created_at
 
     def initialize(performed_action)
-      @actor_id = performed_action['actor_id']
-      @actor_type = performed_action['actor_type']
+      @actor_id = performed_action[:actor_id]
+      @actor_type = performed_action[:actor_type]
       @subject_id = performed_action['subject_id']
       @subject_type = performed_action['subject_type']
       @action = performed_action['action']
