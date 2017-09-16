43071c2b692e6f5db2bd08a2a98c798ec764a870
Anton <anton@ondoc.me>
2017-09-16 18:12:10 +0300

    Fix bug

--- app/dto/performed_action.rb
+++ app/dto/performed_action.rb
@@ -3,12 +3,12 @@ class Dto
     attr_reader :actor_id, :actor_type, :subject_id, :subject_type, :action, :created_at
 
     def initialize(performed_action)
-      @actor_id = performed_action[:actor_id]
-      @actor_type = performed_action[:actor_type]
-      @subject_id = performed_action['subject_id']
-      @subject_type = performed_action['subject_type']
-      @action = performed_action['action']
-      @created_at = performed_action['created_at']
+      @actor_id = performed_action['actor_id'] || performed_action[:actor_id]
+      @actor_type = performed_action['actor_type'] || performed_action[:actor_type]
+      @subject_id = performed_action['subject_id'] || performed_action[:subject_id]
+      @subject_type = performed_action['subject_type'] || performed_action[:subject_type]
+      @action = performed_action['action'] || performed_action[:action]
+      @created_at = performed_action['created_at'] || performed_action[:created_at]
     end
   end
 end
