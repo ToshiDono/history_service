be6c2f3123f9ae7e47c2b0205e827b5f68d53780
Anton <anton@ondoc.me>
2017-09-16 18:01:28 +0300

    Create Dto::PerformedAction

---
+++ app/dto/performed_action.rb
@@ -0,0 +1,14 @@
+class Dto
+  class PerformedAction
+    attr_reader :actor_id, :actor_type, :subject_id, :subject_type, :action, :created_at
+
+    def initialize(performed_action)
+      @actor_id = performed_action['actor_id']
+      @actor_type = performed_action['actor_type']
+      @subject_id = performed_action['subject_id']
+      @subject_type = performed_action['subject_type']
+      @action = performed_action['action']
+      @created_at = performed_action['created_at']
+    end
+  end
+end
